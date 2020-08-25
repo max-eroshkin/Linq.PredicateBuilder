4872bb398d4823eacae78036446d28226014a71c
Max Eroshkin <max.a.eroshkin@gmail.com>
2020-08-25 11:10:55 +0300

    Initial commit

---
+++ source/Linq.PredicateBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq.PredicateBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
