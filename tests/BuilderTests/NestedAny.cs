e0710583deae58e8f2127b9bb1e70470b32c7385
Max Eroshkin <max.a.eroshkin@gmail.com>
2020-09-06 15:43:30 +0300

    Add nested "Any" tests

---
+++ tests/BuilderTests/NestedAny.cs
@@ -0,0 +1,41 @@
+﻿namespace BuilderTests
+{
+    using System.Linq;
+    using FluentAssertions;
+    using Linq.PredicateBuilder;
+    using Xunit;
+    using static Data.TestData;
+
+    public class NestedAny
+    {
+        [Fact]
+        public void NestedIn()
+        {
+            var requestIdList = new long[] { 2, 3 };
+            var result = DataSet.FromBuilder(
+                _ => _.Any(p => p.Requests, n => n.In(x => x.Id, requestIdList)));
+
+            result.Should().BeEquivalentTo(DataSet.Where(x => x.Requests.Any(n => requestIdList.Contains(n.Id))));
+        }
+
+        [Fact]
+        public void NestedInEmpty()
+        {
+            var requestIdList = new long[] { -2, -1 };
+            var result = DataSet.FromBuilder(
+                _ => _.Any(p => p.Requests, n => n.In(x => x.Id, requestIdList)));
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void IgnoreDefault()
+        {
+            long[] requestIdList = null;
+            var result = DataSet.FromBuilder(
+                _ => _.Any(p => p.Requests, n => n.In(x => x.Id, requestIdList)));
+
+            result.Should().BeEquivalentTo(DataSet);
+        }
+    }
+}
--- tests/BuilderTests/Where.cs
+++ tests/BuilderTests/Where.cs
@@ -1,7 +1,5 @@
 ﻿namespace BuilderTests
 {
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
     using FluentAssertions;
     using Linq.PredicateBuilder;
