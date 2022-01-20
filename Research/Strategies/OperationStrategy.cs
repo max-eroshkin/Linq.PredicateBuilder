6c411b30b3e2b40736155a9bc7878a07c8efba10
Max Eroshkin <dealstone@gmail.com>
2022-01-20 18:34:11 +0300

    Added conditional where.

--- Research/Strategies/OperationStrategy.cs
+++ Research/Strategies/OperationStrategy.cs
@@ -1,179 +1,247 @@
-﻿namespace Linq.PredicateBuilder;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using JetBrains.Annotations;
-
-/// <inheritdoc />
-public class OperationStrategy : IOperationStrategy
+﻿namespace Linq.PredicateBuilder
 {
-    private static readonly MethodInfo ToLowerMethod = typeof(string).GetMethods()
-        .First(x => x.Name == nameof(string.ToLower) && x.ToString() == "System.String ToLower()");
-
-    private static readonly MethodInfo ContainsMethod =
-        typeof(string)
-            .GetMethods()
-            .First(x => x.Name == nameof(string.Contains) && x.ToString() == "Boolean Contains(System.String)");
-
-    private static readonly MethodInfo CollectionContainsMethod =
-        typeof(Enumerable)
-            .GetMethods(BindingFlags.Static | BindingFlags.Public)
-            .First(x => x.Name == "Contains" && x.GetParameters().Length == 2);
-
-    private static readonly MethodInfo AnyMethod =
-        typeof(Enumerable)
-            .GetMethods(BindingFlags.Static | BindingFlags.Public)
-            .First(x => x.Name == "Any" && x.GetParameters().Length == 2);
-
-    private readonly BuilderOptions _options;
-
-    /// <summary>
-    /// Constructs a strategy,
-    /// </summary>
-    /// <param name="options">A filtering options</param>
-    public OperationStrategy(BuilderOptions options = BuilderOptions.Default)
-    {
-        _options = options;
-    }
-
-    private bool IgnoreDefaults => (_options & BuilderOptions.IgnoreDefaultInputs) != 0;
-
-    private bool IgnoreCase => (_options & BuilderOptions.IgnoreCase) != 0;
-
-    private bool TrimStrings => (_options & BuilderOptions.Trim) != 0;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using JetBrains.Annotations;
 
     /// <inheritdoc />
-    public Expression<Func<TEntity, bool>> Contains<TEntity>(
-        Expression<Func<TEntity, string>> propertyExpression,
-        string? input)
+    public class OperationStrategy : IOperationStrategy
     {
-        if (IgnoreDefaults && string.IsNullOrWhiteSpace(input))
-            return null;
+        private static readonly MethodInfo ToLowerMethod = typeof(string).GetMethods()
+            .First(x => x.Name == nameof(string.ToLower) && x.ToString() == "System.String ToLower()");
 
-        _ = input ?? throw new ArgumentNullException(nameof(input), "Input cannot be null.");
+        private static readonly MethodInfo ContainsMethod =
+            typeof(string)
+                .GetMethods()
+                .First(x => x.Name == nameof(string.Contains) && x.ToString() == "Boolean Contains(System.String)");
 
-        var filter = Expression.Lambda<Func<TEntity, bool>>(
-            Expression.Call(
-                ToLower(propertyExpression.Body),
-                ContainsMethod,
-                Expression.Constant(ToLower(input), typeof(string))),
-            propertyExpression.Parameters);
+        private static readonly MethodInfo CollectionContainsMethod =
+            typeof(Enumerable)
+                .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .First(x => x.Name == "Contains" && x.GetParameters().Length == 2);
 
-        return filter;
-    }
+        private static readonly MethodInfo AnyMethod =
+            typeof(Enumerable)
+                .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .First(x => x.Name == "Any" && x.GetParameters().Length == 2);
 
-    /// <inheritdoc />
-    public Expression<Func<TEntity, bool>>? StringEquals<TEntity>(
-        Expression<Func<TEntity, string>> propertyExpression,
-        string? input)
-    {
-        if (IgnoreDefaults && string.IsNullOrWhiteSpace(input))
-            return null;
+        private readonly BuilderOptions _options;
 
-        var filter = Expression.Lambda<Func<TEntity, bool>>(
-            Expression.Equal(
-                ToLower(propertyExpression.Body),
-                Expression.Constant(ToLower(input), typeof(string))),
-            propertyExpression.Parameters);
+        /// <summary>
+        /// Constructs a strategy,
+        /// </summary>
+        /// <param name="options">A filtering options.</param>
+        public OperationStrategy(BuilderOptions options = BuilderOptions.Default)
+        {
+            _options = options;
+        }
 
-        return filter;
-    }
+        private bool IgnoreDefaults => (_options & BuilderOptions.IgnoreDefaultInputs) != 0;
 
-    /// <inheritdoc />
-    public Expression<Func<TEntity, bool>>? Equals<TEntity, TValue>(
-        [NotNull] Expression<Func<TEntity, TValue>> propertyExpression,
-        TValue input)
-    {
-        if (IgnoreDefaults && Equals(input, default(TValue)))
-            return null;
+        private bool IgnoreCase => (_options & BuilderOptions.IgnoreCase) != 0;
 
-        if (typeof(TValue) == typeof(string))
-            return StringEquals(propertyExpression as Expression<Func<TEntity, string>>, input as string);
+        private bool TrimStrings => (_options & BuilderOptions.Trim) != 0;
 
-        Expression<Func<TEntity, bool>>? filter = Expression.Lambda<Func<TEntity, bool>>(
-            Expression.Equal(
-                propertyExpression.Body,
-                Expression.Constant(input, typeof(TValue))),
-            propertyExpression.Parameters);
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> Contains<TEntity>(
+            Expression<Func<TEntity, string>> propertyExpression,
+            string input)
+        {
+            if (IgnoreDefaults && string.IsNullOrWhiteSpace(input))
+                return null;
 
-        return filter;
-    }
+            _ = input ?? throw new ArgumentNullException(nameof(input), "Input cannot be null.");
 
-    /// <inheritdoc />
-    public Expression<Func<TEntity, bool>> In<TEntity, TValue>(
-        [NotNull] Expression<Func<TEntity, TValue>> propertyExpression,
-        [CanBeNull] IEnumerable<TValue> input)
-    {
-        _ = propertyExpression ??
-            throw new ArgumentNullException(nameof(propertyExpression), "Expression cannot be null.");
+            var filter = Expression.Lambda<Func<TEntity, bool>>(
+                Expression.Call(
+                    ToLower(propertyExpression.Body),
+                    ContainsMethod,
+                    Expression.Constant(ToLower(input), typeof(string))),
+                propertyExpression.Parameters);
 
-        if (IgnoreDefaults && input?.Any() != true)
-            return null;
+            return filter;
+        }
 
-        _ = input ?? throw new ArgumentNullException(nameof(input), "Input cannot be null.");
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> StringEquals<TEntity>(
+            Expression<Func<TEntity, string>> propertyExpression,
+            string input)
+        {
+            if (IgnoreDefaults && string.IsNullOrWhiteSpace(input))
+                return null;
 
-        var inputParameter = Expression.Constant(
-            typeof(TValue) == typeof(string)
-                ? (IEnumerable<TValue>)((IEnumerable<string?>)input).Select(x => ToLower(x))
-                : input);
+            var filter = Expression.Lambda<Func<TEntity, bool>>(
+                Expression.Equal(
+                    ToLower(propertyExpression.Body),
+                    Expression.Constant(ToLower(input), typeof(string))),
+                propertyExpression.Parameters);
 
-        Expression<Func<TEntity, bool>> filter = Expression.Lambda<Func<TEntity, bool>>(
-            Expression.Call(
-                null,
-                CollectionContainsMethod.MakeGenericMethod(typeof(TValue)),
-                inputParameter,
-                typeof(TValue) == typeof(string)
-                    ? ToLower(propertyExpression.Body)
-                    : propertyExpression.Body),
-            propertyExpression.Parameters);
+            return filter;
+        }
 
-        return filter;
-    }
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> Equals<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, TInput>> propertyExpression,
+            TInput input)
+        {
+            if (IgnoreDefaults && Equals(input, default(TInput)))
+                return null;
 
-    /// <inheritdoc />
-    public Expression<Func<TEntity, bool>> Any<TEntity, TValue>(
-        [NotNull] Expression<Func<TEntity, ICollection<TValue>>> collectionSelector,
-        [CanBeNull] Expression<Func<TValue, bool>> predicate)
-    {
-        _ = collectionSelector ??
-            throw new ArgumentNullException(nameof(collectionSelector), "Expression cannot be null");
+            if (typeof(TInput) == typeof(string))
+                return StringEquals(propertyExpression as Expression<Func<TEntity, string>>, input as string);
 
-        if (predicate == null)
-            return null;
+            Expression<Func<TEntity, bool>> filter = Expression.Lambda<Func<TEntity, bool>>(
+                Expression.Equal(
+                    propertyExpression.Body,
+                    Expression.Constant(input, typeof(TInput))),
+                propertyExpression.Parameters);
 
-        var anyMethod = AnyMethod.MakeGenericMethod(typeof(TValue));
+            return filter;
+        }
 
-        Expression<Func<TEntity, bool>> result = Expression.Lambda<Func<TEntity, bool>>(
-            Expression.Call(
-                null,
-                anyMethod,
-                collectionSelector.Body,
-                predicate),
-            collectionSelector.Parameters);
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> In<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, TInput>> propertyExpression,
+            [CanBeNull] IEnumerable<TInput> input)
+        {
+            _ = propertyExpression ??
+                throw new ArgumentNullException(nameof(propertyExpression), "Expression cannot be null.");
 
-        return result;
-    }
+            if (IgnoreDefaults && input?.Any() != true)
+                return null;
 
-    private Expression ToLower(Expression property)
-    {
-        return IgnoreCase
-            ? Expression.Call(property, ToLowerMethod)
-            : property;
-    }
+            _ = input ?? throw new ArgumentNullException(nameof(input), "Input cannot be null.");
 
-    private string? ToLower(string? input)
-    {
-        if (input == null)
-            return null;
+            var inputParameter = Expression.Constant(
+                typeof(TInput) == typeof(string)
+                    ? (IEnumerable<TInput>)((IEnumerable<string>)input).Select(x => ToLower(x))
+                    : input);
 
-        if (TrimStrings)
-            input = input.Trim();
+            Expression<Func<TEntity, bool>> filter = Expression.Lambda<Func<TEntity, bool>>(
+                Expression.Call(
+                    null,
+                    CollectionContainsMethod.MakeGenericMethod(typeof(TInput)),
+                    inputParameter,
+                    typeof(TInput) == typeof(string)
+                        ? ToLower(propertyExpression.Body)
+                        : propertyExpression.Body),
+                propertyExpression.Parameters);
 
-        return IgnoreCase
-            ? input.ToLower()
-            : input;
+            return filter;
+        }
+
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> Any<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, ICollection<TInput>>> collectionSelector,
+            [CanBeNull] Expression<Func<TInput, bool>> predicate)
+        {
+            _ = collectionSelector ??
+                throw new ArgumentNullException(nameof(collectionSelector), "Expression cannot be null");
+
+            if (predicate == null)
+                return null;
+
+            var anyMethod = AnyMethod.MakeGenericMethod(typeof(TInput));
+
+            Expression<Func<TEntity, bool>> result = Expression.Lambda<Func<TEntity, bool>>(
+                Expression.Call(
+                    null,
+                    anyMethod,
+                    collectionSelector.Body,
+                    predicate),
+                collectionSelector.Parameters);
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> Where<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, TInput, bool>> predicate,
+            TInput input)
+        {
+            if (IgnoreDefaults && Equals(input, default(TInput)))
+                return null;
+
+            if (IgnoreDefaults && input is IEnumerable enumerable)
+            {
+                var enumerator = enumerable.GetEnumerator();
+                var disposable = enumerator as IDisposable;
+                try
+                {
+                    if (!enumerator.MoveNext())
+                        return null;
+                }
+                finally
+                {
+                    disposable?.Dispose();
+                }
+            }
+
+            if (typeof(TInput) == typeof(string))
+                return StringWhere(predicate as Expression<Func<TEntity, string, bool>>, input as string);
+
+            var replacement = new Dictionary<ParameterExpression, Expression>
+            {
+                { predicate.Parameters.Last(), Expression.Constant(input, typeof(TInput)) }
+            };
+
+            var body = ParameterRebinder.ReplaceParameters(
+                replacement,
+                predicate.Body);
+
+            var filter = Expression.Lambda<Func<TEntity, bool>>(
+                body,
+                predicate.Parameters.First());
+
+            return filter;
+        }
+
+        /// <inheritdoc cref="Where{TEntity,TInput}"/>
+        private Expression<Func<TEntity, bool>> StringWhere<TEntity>(
+            [NotNull] Expression<Func<TEntity, string, bool>> predicate,
+            string input)
+        {
+            if (IgnoreDefaults && string.IsNullOrWhiteSpace(input))
+                return null;
+
+            var replacement = new Dictionary<ParameterExpression, Expression>
+            {
+                { predicate.Parameters.Last(), Expression.Constant(ToLower(input), typeof(string)) }
+            };
+
+            var body = ParameterRebinder.ReplaceParameters(
+                replacement,
+                predicate.Body);
+
+            var filter = Expression.Lambda<Func<TEntity, bool>>(
+                body,
+                predicate.Parameters.First());
+
+            return filter;
+        }
+
+        private Expression ToLower(Expression property)
+        {
+            return IgnoreCase
+                ? Expression.Call(property, ToLowerMethod)
+                : property;
+        }
+
+        private string ToLower(string input)
+        {
+            if (input == null)
+                return null;
+
+            if (TrimStrings)
+                input = input.Trim();
+
+            return IgnoreCase
+                ? input.ToLower()
+                : input;
+        }
     }
 }
---
+++ Research/Strategies/ParameterRebinder.cs
@@ -0,0 +1,44 @@
+﻿namespace Linq.PredicateBuilder
+{
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Parameter rebinder.
+    /// </summary>
+    internal sealed class ParameterRebinder : ExpressionVisitor
+    {
+        private readonly Dictionary<ParameterExpression, Expression> _map;
+
+        private ParameterRebinder(Dictionary<ParameterExpression, Expression> map)
+        {
+            _map = map ?? new Dictionary<ParameterExpression, Expression>();
+        }
+
+        /// <summary>
+        /// Replaces parameter in the expression.
+        /// </summary>
+        /// <param name="map">Parameter map.</param>
+        /// <param name="exp">An expression.</param>
+        public static Expression ReplaceParameters(
+            Dictionary<ParameterExpression, Expression> map,
+            Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        /// <inheritdoc />
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            if (_map.TryGetValue(p, out var replacement))
+            {
+                if (replacement is ParameterExpression pe)
+                    p = pe;
+                else
+                    return replacement;
+            }
+
+            return base.VisitParameter(p);
+        }
+    }
+}
