bfc8acbc8155d8278ab7ab12470b2e74c0a65330
Max Eroshkin <dealstone@gmail.com>
2022-01-20 18:34:20 +0300

    Added conditional where.

--- Research/Strategies/IOperationStrategy.cs
+++ Research/Strategies/IOperationStrategy.cs
@@ -1,66 +1,48 @@
-﻿namespace Linq.PredicateBuilder;
-
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-
-/// <summary>
-/// Contains filtering methods.
-/// </summary>
-public interface IOperationStrategy
+﻿namespace Linq.PredicateBuilder
 {
-    /// <summary>
-    /// Contains predicate
-    /// </summary>
-    /// <param name="propertyExpression">Property selector</param>
-    /// <param name="input">string to find</param>
-    /// <typeparam name="TEntity">Entity type</typeparam>
-    /// <returns></returns>
-    Expression<Func<TEntity, bool>> Contains<TEntity>(
-        Expression<Func<TEntity, string>> propertyExpression,
-        string? input);
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using JetBrains.Annotations;
 
     /// <summary>
-    /// Contains predicate
+    /// Constructs filtering methods.
     /// </summary>
-    /// <param name="propertyExpression">Property selector</param>
-    /// <param name="input">string to find</param>
-    /// <typeparam name="TEntity">Entity type</typeparam>
-    /// <returns></returns>
-    Expression<Func<TEntity, bool>>? StringEquals<TEntity>(
-        Expression<Func<TEntity, string>> propertyExpression,
-        string? input);
+    public interface IOperationStrategy
+    {
+        /// <inheritdoc cref="ILogicOperationT{TBuilderResult,TEntity}.Contains"/>
+        [CanBeNull]
+        Expression<Func<TEntity, bool>> Contains<TEntity>(
+            Expression<Func<TEntity, string>> propertyExpression,
+            string input);
 
-    /// <summary>
-    /// Contains predicate
-    /// </summary>
-    /// <param name="propertyExpression">Property selector</param>
-    /// <param name="input">string to find</param>
-    /// <typeparam name="TEntity">Entity type</typeparam>
-    /// <typeparam name="TValue">Тип свойства</typeparam>
-    Expression<Func<TEntity, bool>>? Equals<TEntity, TValue>(
-        Expression<Func<TEntity, TValue>> propertyExpression,
-        TValue input);
+        /// <inheritdoc cref="ILogicOperationT{TBuilderResult,TEntity}.Equals{TInput}"/>
+        [CanBeNull]
+        Expression<Func<TEntity, bool>> StringEquals<TEntity>(
+            Expression<Func<TEntity, string>> propertyExpression,
+            string input);
 
-    /// <summary>
-    /// Contains predicate
-    /// </summary>
-    /// <param name="propertyExpression">Property selector</param>
-    /// <param name="input">string to find</param>
-    /// <typeparam name="TEntity">Entity type</typeparam>
-    /// <typeparam name="TValue">Тип свойства</typeparam>
-    Expression<Func<TEntity, bool>>? In<TEntity, TValue>(
-        Expression<Func<TEntity, TValue>> propertyExpression,
-        IEnumerable<TValue>? input);
+        /// <inheritdoc cref="ILogicOperationT{TBuilderResult,TEntity}.Equals{TInput}"/>
+        Expression<Func<TEntity, bool>> Equals<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, TInput>> propertyExpression,
+            TInput input);
 
-    /// <summary>
-    /// Contains predicate
-    /// </summary>
-    /// <param name="collectionSelector">Many-to-many Property selector</param>
-    /// <param name="predicate">Property selector</param>
-    /// <typeparam name="TEntity">Entity type</typeparam>
-    /// <typeparam name="TValue">Тип свойства коллекции many-to-many</typeparam>
-    Expression<Func<TEntity, bool>>? Any<TEntity, TValue>(
-        Expression<Func<TEntity, ICollection<TValue>>> collectionSelector,
-        Expression<Func<TValue, bool>>? predicate);
+        /// <inheritdoc cref="ILogicOperationT{TBuilderResult,TEntity}.In{TInput}"/>
+        [CanBeNull]
+        Expression<Func<TEntity, bool>> In<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, TInput>> propertyExpression,
+            [CanBeNull] IEnumerable<TInput> input);
+
+        /// <inheritdoc cref="ILogicOperationT{TBuilderResult,TEntity}.Where"/>
+        [CanBeNull]
+        Expression<Func<TEntity, bool>> Any<TEntity, TInput>(
+            [NotNull] Expression<Func<TEntity, ICollection<TInput>>> collectionSelector,
+            [CanBeNull] Expression<Func<TInput, bool>> predicate);
+
+        /// <inheritdoc cref="ILogicOperationT{TBuilderResult,TEntity}.Where"/>
+        [CanBeNull]
+        Expression<Func<TEntity, bool>> Where<TEntity, TInput>(
+            Expression<Func<TEntity, TInput, bool>> predicate,
+            TInput input);
+    }
 }
