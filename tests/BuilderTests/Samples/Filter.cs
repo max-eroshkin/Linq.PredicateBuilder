f7a8b814a189b1be89d3bf57f1053c013441a4f5
Max Eroshkin <dealstone@gmail.com>
2022-04-12 03:52:26 +0700

    Fixes

--- tests/BuilderTests/Samples/Filter.cs
+++ tests/BuilderTests/Samples/Filter.cs
@@ -5,17 +5,17 @@ using System.Collections.Generic;
 
 public class Filter
 {
-    public string FirstName { get; set; }
+    public string? FirstName { get; set; }
 
-    public string LastName { get; set; }
+    public string? LastName { get; set; }
 
-    public string Comment { get; set; }
+    public string? Comment { get; set; }
 
     public DateOnly? DateOfBirth { get; set; }
 
     public Gender? Gender { get; set; }
 
-    public List<int> Ids { get; set; }
+    public List<int>? Ids { get; set; }
 
     public bool? HasRelatives { get; set; }
 }
--- tests/BuilderTests/Samples/Person.cs
+++ tests/BuilderTests/Samples/Person.cs
@@ -9,9 +9,9 @@ public class Person
 {
     public int Id { get; set; }
 
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
 
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
 
     public DateOnly? DateOfBirth { get; set; }
 
